e11ce1e7ce01f0912c46bdf29dcf104e6ebf82c4
adedotun <dotun.ogunwuyi71@gmail.com>
2023-12-10 13:19:34 -0500

    Adding Naman's files

---
+++ Event Final Project/MainWindow.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Event_Final_Project
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnClickRegister(object sender, RoutedEventArgs e)
+        {
+            Main.Content = new Register();
+        }
+        private void BtnClickProducts(object sender, RoutedEventArgs e)
+        {
+            Main.Content = new Products();
+        }
+        private void BtnClickFeedback(object sender, RoutedEventArgs e)
+        {
+            Main.Content = new Feedback();
+        }
+        private void BtnClickContact(object sender, RoutedEventArgs e)
+        {
+            Main.Content = new Contact();
+        }
+        private void BtnClickAbout(object sender, RoutedEventArgs e)
+        {
+            Main.Content = new About();
+        }
+    }
+}
